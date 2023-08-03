1116e33ade09d0f4ee5b2f0ad5f906a4efef6932
Teten Nugraha <teten.nugraha18@gmail.com>
2023-08-03 08:29:48 +0700

    first commit

---
+++ src/main/java/id/ten/springdynamicspecquery/SpringDynamicSpecQueryApplication.java
@@ -0,0 +1,13 @@
+package id.ten.springdynamicspecquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDynamicSpecQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDynamicSpecQueryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ten/springdynamicspecquery/SpringDynamicSpecQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ten.springdynamicspecquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDynamicSpecQueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
